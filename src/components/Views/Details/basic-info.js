288ff4a381e229f74cdca277630852be5fe92fc7
claudiaton <claudiatucunduva@gmail.com>
2022-04-14 02:45:51 -0300

    included flags in details info component

--- src/components/Views/Details/basic-info.js
+++ src/components/Views/Details/basic-info.js
@@ -2,7 +2,7 @@
 // import ShowContext from '../Context/show-context';
 // import { useApi } from '../Context/show-context';
 import { Card } from 'react-bootstrap';
-
+import Flag from 'react-world-flags'
 
 const BasicInfo = function (props) {
 
@@ -14,7 +14,7 @@ const BasicInfo = function (props) {
             {data && (
                 <>
                     <Card.Title>Show Info</Card.Title>
-                    <Card.Text>Network: {data.network.name} {data.network.code}</Card.Text>
+                    <Card.Text>Network: {data.network.name} <Flag code={data.network.country.code} width='24px'/>{data.network.country.code}</Card.Text>
                     <Card.Text>Official site: <Card.Link href={data.officialSite}>{data.officialSite}</Card.Link></Card.Text>
                     <Card.Text>Pemiered: {data.premiered}</Card.Text>
                     <Card.Text>Status: {data.status}</Card.Text>
